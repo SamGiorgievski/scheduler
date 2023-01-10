85d34c459794ca870dff4abfa69a7cd02549e29d
Samuel Giorgievski <samgiorgievski@gmail.com>
2023-01-10 18:59:32 -0500

    update text display on dayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,6 +4,16 @@ import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
 
+  const FormatSpots = () => {
+    return(
+      <div>
+      {props.spots === 0 && <h3 className="text--light">no spots remaining</h3>}
+      {props.spots === 1 && <h3 className="text--light">1 spot remaining</h3>}
+      {props.spots > 1 && <h3 className="text--light">{props.spots} spots remaining</h3>}
+      </div>
+    );
+  };
+
   const dayListClass = classNames(
     'day-list',
     'day-list__item',
@@ -15,7 +25,7 @@ export default function DayListItem(props) {
     <li className = {dayListClass} 
     onClick={() => {props.setDay(props.name)}}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <FormatSpots/>
     </li>
   );
 }
