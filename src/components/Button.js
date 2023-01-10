f56cbbb5c5b56700480c09ec58a5c4186d9fb214
Samuel Giorgievski <samgiorgievski@gmail.com>
2023-01-10 16:43:33 -0500

    create button with props and eventhandling

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,26 @@
 import React from "react";
-
+import classNames from "classnames";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+
+   let buttonClass = classNames(
+      'button',
+      {'button--confirm': props.confirm},
+      {'button--danger': props.danger},
+      {'onClick': props.onClick},
+      {'disabled': props.disabled}
+      );
+
+   return (
+   <>
+      <button 
+      className = {buttonClass} 
+      onClick={props.onClick} 
+      disabled={props.disabled}
+      >
+         {props.children}
+      </button>
+   </>
+   );
 }
