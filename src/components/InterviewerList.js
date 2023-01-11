34c58bd0a16dcd68f6932a47f01a8cfecfa4d31f
Samuel Giorgievski <samgiorgievski@gmail.com>
2023-01-11 15:49:11 -0500

    add InterviewerList

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -4,21 +4,22 @@ import "./InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
 
 
-// const interviewers = [
-//   { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-//   { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-//   { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-//   { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-//   { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
-// ];
+const interviewers = [
+  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+];
 
 export default function InterviewerList(props) {
 
-  const renderInterviewerList = props.days.map((interviewer) => {
+  const renderInterviewerList = interviewers.map((interviewer) => { 
     return (
       
       <InterviewerListItem 
           key={interviewer.id}
+          id={interviewer.id}
           name={interviewer.name} 
           avatar={interviewer.avatar} 
           selected={interviewer.id === props.interviewer}
@@ -31,7 +32,7 @@ export default function InterviewerList(props) {
 return (
 <section className="interviewers">
   <h4 className="interviewers__header text--light">Interviewer</h4>
-  <ul className="interviewers__list"></ul>
+  <ul className="interviewers__list">{renderInterviewerList}</ul>
 </section>
 );
 
