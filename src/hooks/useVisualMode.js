190c04c4bd4e5190e9dab82552b8548880988fdb
Samuel Giorgievski <samgiorgievski@gmail.com>
2023-01-16 15:31:37 -0500

    create and test custom hook for appointments

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(next, replace = false) {
+    
+    if (replace) {
+      history.pop();
+    }
+
+    history.push(next);
+    setHistory(history);
+    setMode(next);
+
+  }
+
+  function back() {
+
+    if (history.length > 1) {
+      history.pop();
+      setHistory(history);
+      setMode(history[history.length-1]);
+    } 
+  }
+
+  return { mode, transition, back };
+}
