c80eb276a25ce4484a11400b0f0fcbd1fa8b761b
Samuel Giorgievski <samgiorgievski@gmail.com>
2023-01-17 19:55:50 -0500

    add form tests and error messages

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,7 @@ export default function Form(props) {
 
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
 
   const reset = function () {
@@ -18,6 +19,21 @@ export default function Form(props) {
     props.onCancel();
   }
 
+  function validate() {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+
+    if (interviewer === null) {
+      setError("Please select an interviewer");
+      return;
+    }
+  
+    props.onSave(student, interviewer);
+  }
+
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -26,11 +42,13 @@ export default function Form(props) {
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
-            placeholder= "Enter student name"
+            placeholder= "Enter Student Name"
             onChange={(event) => setStudent(event.target.value)}
             value={student}
+            data-testid="student-name-input"
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList
         onChange={setInterviewer}
         value={interviewer}
@@ -40,7 +58,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick = {cancel}>Cancel</Button>
-          <Button confirm onClick = {() => props.onSave(student, interviewer)}>Save</Button>
+          <Button confirm onClick = {() => validate()}>Save</Button>
         </section>
       </section>
     </main>
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
+import PropTypes from 'prop-types';
 
 
 export default function InterviewerList(props) {
